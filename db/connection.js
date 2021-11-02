d5644a533c63bda2f468a88e09afd3f8174f9b99
Ivon <ivonne@jjf.com>
2021-11-02 17:33:38 -0500

    added voter routes

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+//import mysql2 package
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'password',
+    database: 'election'
+  });
+
+  module.exports = db;
